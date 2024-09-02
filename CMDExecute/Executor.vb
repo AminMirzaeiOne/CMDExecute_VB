7806f88cfa9035714cba41d28eb39e2668acd80d
Amin Mirzaei <aminmirzaei.og@gmail.com>
2024-09-02 05:42:07 +0330

    - Create Async ExecuteAsync Function

--- CMDExecute/Executor.vb
+++ CMDExecute/Executor.vb
@@ -1,3 +1,22 @@
 ﻿Public Class Executor
+    Public Async Function ExecuteAsync(userCommand As String) As System.Threading.Tasks.Task
+        Try
+            Await System.Threading.Tasks.Task.Run(Sub()
+                                                      Using cmd As New Process()
+                                                          cmd.StartInfo.FileName = "cmd.exe"
+                                                          cmd.StartInfo.RedirectStandardInput = True
+                                                          cmd.StartInfo.RedirectStandardOutput = True
+                                                          cmd.StartInfo.CreateNoWindow = True
+                                                          cmd.StartInfo.UseShellExecute = False
+                                                          cmd.Start()
 
+                                                          cmd.StandardInput.WriteLine(userCommand)
+                                                          cmd.StandardInput.Flush()
+                                                          cmd.StandardInput.Close()
+                                                          cmd.WaitForExit()
+                                                      End Using
+                                                  End Sub)
+        Catch
+        End Try
+    End Function
 End Class
